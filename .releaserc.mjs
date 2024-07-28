764757432bc30f884c6eb7bcdcb189a24e37387e
Igor Golovin <lislon@mail.ru>
2024-07-28 17:07:44 -0500

    fix: add plugin

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -5,6 +5,7 @@ export default {
   plugins: [
     // to get docker container export image
     // https://github.com/felipecrs/semantic-release-export-data#github-actions-example
-    'semantic-release-export-data'
+    'semantic-release-export-data',
+    '@semantic-release/github'
   ]
 };
