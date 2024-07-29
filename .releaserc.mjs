062218a48a45664a761f74b87f6cd6a504fe92b8
Igor Golovin <lislon@mail.ru>
2024-07-28 20:06:40 -0500

    fix: more messages

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -5,6 +5,8 @@ export default {
   plugins: [
     // to get docker container export image
     // https://github.com/felipecrs/semantic-release-export-data#github-actions-example
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     'semantic-release-export-data',
     '@semantic-release/github'
   ]
