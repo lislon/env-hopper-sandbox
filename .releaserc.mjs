4730f306544ce74a364d204db6fe461905a067ae
Igor Golovin <lislon@mail.ru>
2024-07-28 20:33:21 -0500

    chore: test

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -3,7 +3,7 @@
  */
 export default {
   plugins: [
-    // to get docker container export image
+    // to get docker container export image 2
     // https://github.com/felipecrs/semantic-release-export-data#github-actions-example
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
